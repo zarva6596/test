9f44098da723720f096d0695408843ae4b50ac6b
zarva6596 <zarva6596@gmail.com>
2024-02-21 23:53:14 +0200

    second commit

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,20 +3,20 @@
 </template>
 
 <script>
-paypal.Buttons({
-  style: {
-    shape: 'pill',
-    color: 'blue',
-    layout: 'vertical',
-    label: 'subscribe',
-  },
-  createSubscription: function(data, actions) {
-    return actions.subscription.create({
-      plan_id: 'P-8GL91717W4466305WMXLGEII',
-    });
-  },
-  onApprove: function(data) {
-    alert(data.subscriptionID);
-  },
-}).render('#paypal-button-container');
+// paypal.Buttons({
+//   style: {
+//     shape: 'pill',
+//     color: 'blue',
+//     layout: 'vertical',
+//     label: 'subscribe',
+//   },
+//   createSubscription: function(data, actions) {
+//     return actions.subscription.create({
+//       plan_id: 'P-8GL91717W4466305WMXLGEII',
+//     });
+//   },
+//   onApprove: function(data) {
+//     alert(data.subscriptionID);
+//   },
+// }).render('#paypal-button-container');
 </script>
